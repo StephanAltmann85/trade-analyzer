bb4ee0dd3f7ce54902fefe6e2f78bad22c5556d7
Stephan Altmann <salty@Lackmaus.local>
2018-05-05 21:04:45 +0200

    basic implementation for connections

--- app/src/Services/HitBTC/ConnectionService.php
+++ app/src/Services/HitBTC/ConnectionService.php
@@ -3,15 +3,28 @@
 namespace App\Services\HitBTC;
 
 use App\Services\ConnectionServiceInterface;
+use Unirest;
 
 class ConnectionService implements ConnectionServiceInterface {
 
-    public function __construct() {
+    public $serviceBaseUrl;
 
+    public function __construct(string $serviceBaseUrl) {
+        $this->serviceBaseUrl = $serviceBaseUrl;
     }
 
     public function get(array $params) {
-        return 'test';
+        $url = $this->serviceBaseUrl . $params['endpoint'] . '/' . $params['symbol'];
+
+        $headers = array('Accept' => 'application/json');
+
+        $data = $params['data'];
+
+        $body = Unirest\Request\Body::form($data);
+
+        $response = Unirest\Request::get($url, $headers, $body);
+
+        return $response;
     }
 
 
--- app/src/Services/HitBTC/RequestService.php
+++ app/src/Services/HitBTC/RequestService.php
@@ -3,38 +3,43 @@
 namespace App\Services\HitBTC;
 
 use App\DTO\OrderBook;
+use App\Services\ConnectionServiceInterface;
 use App\Services\RequestServiceInterface;
 use App\DTO\AbstractDTO;
+use Unirest\Response;
 
 class RequestService implements RequestServiceInterface {
 
     private $connectionService;
+    private $service;
 
-    public function __construct(ConnectionService $connectionService) {
+    public function __construct(ConnectionServiceInterface $connectionService, string $service) {
         $this->connectionService = $connectionService;
+        $this->service = $service;
     }
 
     //Todo: return array column of dto
     public function getOrderBook(string $symbol) {
+
         $params = array(
             'endpoint' => 'orderbook',
-            'symbol' => $symbol
+            'symbol' => $symbol,
+            'data' => array()
         );
 
         $response = $this->connectionService->get($params);
         return $this->transform(OrderBook::class, $response);
     }
 
-    private function transform(string $className, string $response) {
+    private function transform(string $className, Response $response) {
 
-        //TODO: decode
         //TODO: implement handler based on endpoint
 
-        $test = new $className($response, 'HitBTC');
+        //$test = new $className($response, $this->service);
         //iterate -> fill arraycolum with dto
 
 
-        die(var_dump($test));
+        die(var_dump($response));
 
     }
 }
