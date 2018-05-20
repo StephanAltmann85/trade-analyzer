64409e11bd225753d0bf3a47aacdcdb0e1d90114
Stephan Altmann <salty@Lackmaus.local>
2018-05-21 00:10:48 +0200

    added notes for implementing tests

--- app/tests/RequestTest.php
+++ app/tests/RequestTest.php
@@ -6,8 +6,21 @@ use PHPUnit\Framework\TestCase;
 
 class RequestTest extends TestCase
 {
-    public function testSomething()
-    {
-        $this->assertTrue(true);
-    }
+
+    //TODO: test request service selector
+
+    //TODO: test connection
+    //TODO: test request
+
+    //TODO: test request assign
+    //TODO: test request transform
+
+    //TODO: test dto compatibility
+
+    //TODO: test error handling
+        //wrong service
+        //no response
+        //wrong response statuscode
+        //wrong response structure
+        //
 }
